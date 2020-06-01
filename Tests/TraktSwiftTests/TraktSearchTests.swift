1aee40c3a421e0a5de50738f0baa39604c0fa54b
martinpucik <martin.pucik@me.com>
2020-06-01 20:55:41 +0200

    Search tests combine

--- Tests/TraktSwiftTests/TraktSearchTests.swift
+++ Tests/TraktSwiftTests/TraktSearchTests.swift
@@ -12,6 +12,7 @@ import XCTest
 final class TraktSearchTests: XCTestCase {
     static var allTests = [
         ("testSearch", testSearch),
+        ("testSearchCombine", testSearchCombine)
     ]
 
     override class func setUp() {
@@ -33,6 +34,29 @@ final class TraktSearchTests: XCTestCase {
         })
         XCTAssertEqual(request.originalRequest?.url?.path, "/search/movie")
         XCTAssertEqual(request.originalRequest?.url?.query, "query=tron%20legacy")
+        wait(for: [expectation], timeout: 5)
+    }
+
+    func testSearchCombine() {
+        let expectation = XCTestExpectation()
+        let request = Trakt.search(query: "tron legacy", in: [.movie])
+            .sink(receiveCompletion: { result in
+
+            }, receiveValue: { response in
+                XCTAssertFalse(response.results.isEmpty)
+            })
+
+//        search(query: "tron legacy", in: [.movie], completion: { result in
+//            switch result {
+//                case .success(let response):
+//                    XCTAssertFalse(response.results.isEmpty)
+//                    expectation.fulfill()
+//                case .failure(let error):
+//                    XCTFail(error.localizedDescription)
+//            }
+//        })
+//        XCTAssertEqual(request.originalRequest?.url?.path, "/search/movie")
+//        XCTAssertEqual(request.originalRequest?.url?.query, "query=tron%20legacy")
         wait(for: [expectation], timeout: 5)
     }
 }
